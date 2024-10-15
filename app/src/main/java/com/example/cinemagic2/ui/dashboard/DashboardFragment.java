b906348917bcfa48df50fe241253e32c394d356e
admin <dkachhadiya303@gmail.com>
2024-10-15 21:53:13 +0530

    ,,

--- app/src/main/java/com/example/cinemagic2/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/cinemagic2/ui/dashboard/DashboardFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -47,7 +48,6 @@ public class DashboardFragment extends Fragment {
 
         return root;
     }
-
     @Override
     public void onDestroyView() {
         super.onDestroyView();
